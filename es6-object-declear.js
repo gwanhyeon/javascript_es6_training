924351b09673ab5e915636bcd79ab092301f06bf
kgh940525 <kgh940525@gmail.com>
2019-04-16 03:15:50 +0900

    [Object declear] setter,getter

---
+++ es6-object-declear.js
@@ -0,0 +1,35 @@
+// object생성
+
+const name = 'kgh';
+const age = '26';
+// Object = key : value
+const user = {
+    name : name,
+    age : age
+}
+console.log(user);
+
+function getObj() {
+    const name = 'crong';
+    const getName = function() {
+        return name;
+    }
+    const setName = function(newname){
+        name = newname;
+    }
+    const printName = function() {
+        console.log(name);
+    }
+    //todo  굳이 key value 가 일치하면 
+    return {
+
+        //todo {getName, setname} 도 동일하게 처리 된다!
+        getName : getName,
+        setName : setName
+    }
+}
+let obj = getObj();
+console.log('obj' ,obj);
+//todo { getName: [Function: getName], setName: [Function: setName] }
+console.log('obj.getName()',obj.getName());
+//todo
